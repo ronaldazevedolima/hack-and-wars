7ee1106c52dabbadb01e44906aab1de25e1a1abd
Ronald Lima <ronaldlima@MacBook-Pro-de-Ronald.local>
2025-01-29 13:20:19 -0300

    configurações iniciais

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    {languageOptions: { globals: globals.browser }},
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            'semi': ['error', 'always'],
+            'quotes': ['error', 'single'],
+            'indent': ['error', 4],
+        }
+    }
+];
